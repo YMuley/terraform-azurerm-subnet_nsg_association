5383209f5332da5be153f6e8a037a8ad67dc9202
sunandasharma61 <sunandasharma60101@gmail.com>
2023-11-02 14:10:57 +0530

    testing

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  network_security_group={for network_security_group in var.association_list:nsg.nsg_name=>nsg}
+  network_security_group = { for network_security_group in var.association_list : network_security_group.nsg_name => network_security_group }
 }
