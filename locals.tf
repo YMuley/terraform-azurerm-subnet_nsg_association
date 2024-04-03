5088d3a8d22c6021d4dc80944ebede12d547dbe0
sunanda sharma <sunandasharma@ddiworld.com>
2024-04-03 17:41:40 +0530

    updated

--- locals.tf
+++ locals.tf
@@ -1,3 +1,7 @@
+# locals {
+#   network_security_group = { for network_security_group in var.subnet_nsg_association_list : network_security_group.nsg_name => network_security_group }
+# }
+
 locals {
-  network_security_group = { for network_security_group in var.subnet_nsg_association_list : network_security_group.nsg_name => network_security_group }
+  network_security_group = { for network_security_group in var.subnet_nsg_association_list : network_security_group.nsg_name => [network_security_group...] }
 }
