30285170e9197f51b16f5daabf7157adf4b62513
sunanda sharma <sunandasharma@ddiworld.com>
2024-04-04 12:44:55 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 #----------- Network Security Group - Virtual Network Association
 resource "azurerm_subnet_network_security_group_association" "subnet_network_security_group_association" {
-  for_each  = local.network_security_group
-  subnet_id = var.subnet_output[each.key].id
-  #var.subnet_output[format("%s/%s", each.key.virtual_network_name, each.key.subnet_name)].id //var.subnet_output[each.value.subnet_id].id
-  network_security_group_id = var.network_security_group_output[each.value].id
+  for_each                  = local.network_security_group
+  subnet_id                 = var.subnet_output[each.key].id //var.subnet_output[format("%s/%s", each.key.virtual_network_name, each.key.subnet_name)].id
+  network_security_group_id = var.network_security_group_output[each.key].id
 }
