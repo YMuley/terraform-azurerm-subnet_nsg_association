725bc896539110f96a3f600ca520e17a273ceae1
sunanda sharma <sunandasharma@ddiworld.com>
2024-04-04 13:03:13 +0530

    updated

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@
 resource "azurerm_subnet_network_security_group_association" "subnet_network_security_group_association" {
   for_each                  = local.network_security_group
   subnet_id                 = var.subnet_output[each.key].id //var.subnet_output[format("%s/%s", each.value.virtual_network_name, each.value.subnet_name)].id 
-  network_security_group_id = var.network_security_group_output[each.value.nsg_name].id
+  network_security_group_id = var.network_security_group_output[each.value].id
 }
