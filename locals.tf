482408074b190b4e827b524232180090f2c3679a
sunanda sharma <sunandasharma@ddiworld.com>
2024-04-05 11:50:15 +0530

    updated

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
-# locals {
-#   network_security_group = { for network_security_group in var.subnet_nsg_association_list : network_security_group.nsg_name => network_security_group }
-# }
-
 locals {
-  network_security_group = { for network_security_group in var.subnet_nsg_association_list : format("%s-%s-%s", network_security_group.nsg_name, network_security_group.subnet_name, network_security_group.virtual_network_name) => [network_security_group] }
+  network_security_group = { for network_security_group in var.subnet_nsg_association_list : network_security_group.nsg_name => network_security_group }
 }
+
+# locals {
+#   network_security_group = { for network_security_group in var.subnet_nsg_association_list : format("%s-%s-%s", network_security_group.nsg_name, network_security_group.subnet_name, network_security_group.virtual_network_name) => [network_security_group] }
+# }
